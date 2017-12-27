55607916baa22e9975e5634567948efd2222796a
Cesar Chen <cesar6134@gmail.com>
2017-12-27 11:34:27 +0800

    Update test file

--- test/createReducer.spec.js
+++ test/createReducer.spec.js
@@ -13,5 +13,19 @@ describe('Utils', () => {
       const s2 = reducer(s1, { type: 'inc' })
       expect(s2).toEqual(1)
     })
+
+    it('throw error when the first argument is not an Array', () => {
+
+      expect(() => createReducer({}, 0)).toThrow()
+    })
+
+    it('throw error when the element of the pairs is not a tuple', () => {
+      const reducer = createReducer([
+        ['inc', state => state + 1]
+        ['dec', state => state - 1]
+      ], 0)
+
+      expect(() => reducer(undefined, {})).toThrow()
+    })
   })
 })
