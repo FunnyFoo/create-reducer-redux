183b8f6e8d074cea8f706e44fa069e8a8b5eee83
Cesar Chen <cesar6134@gmail.com>
2019-05-20 01:45:03 +0800

    build: add package to analyze the bundled file size

--- rollup.config.js
+++ rollup.config.js
@@ -3,12 +3,13 @@ import commonjs from 'rollup-plugin-commonjs'
 import typescript from 'rollup-plugin-typescript'
 import replace from 'rollup-plugin-replace'
 import { uglify } from 'rollup-plugin-uglify'
+import { sizeSnapshot } from 'rollup-plugin-size-snapshot'
 
 const ENV = process.env.NODE_ENV
 
 const config = {
   input: 'src/index.ts',
-  plugins: []
+  plugins: [sizeSnapshot()]
 }
 
 if (ENV === 'es' || ENV === 'cjs') {
